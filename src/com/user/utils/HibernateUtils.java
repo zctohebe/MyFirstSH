99e1898087b15c470d146e09a9839dd5a4c18f57
zctohebe <411208475@qq.com>
2019-02-13 21:37:34 +0800

    Revert "Develop2"

--- src/com/user/utils/HibernateUtils.java
+++ src/com/user/utils/HibernateUtils.java
@@ -12,13 +12,11 @@ public class HibernateUtils {
 	}
 
 	public static Session openSession() {
-		//asdjhj
 		Session session = sessionFactory.openSession();
 		return session;
 	}
 
 	public static void closeSession(Session session) {
-		//asdlh
 		if (null != session) {
 			session.close();
 		}
